b5e7ec75021ed7c2aaa10a8c73d3ac189b4925a9
nalvarado15 <131101011+nalvarado15@users.noreply.github.com>
2024-11-14 14:26:52 -0800

    Update Dealer.java
    We need to change the class diagrams.
    - Dealers will hold tables (Dealers will create and hold games)
    - Dealers will deal cards through accessing the table>shoe>card
    - We don't have to worry about seen or unseen cards
    - We need to establish a method/ attribute in table to hold the array/ list of players to know WHO to give the card to

--- BlackjackGame/shared/src/model/Dealer.java
+++ BlackjackGame/shared/src/model/Dealer.java
@@ -1,11 +1,12 @@
 package model;
 
 public class Dealer {
-	private Card seenCard;
-	private Card unseenCard;
+	//private Card seenCard;
+	//private Card unseenCard;
 	private Hand dealerHand;
 	private Table table;
 	private Lobby lobby; //get Lobby from server class
+	private Card card;
 	
 	public Dealer(Lobby lobby, int minBet) {
 		this.lobby  = lobby;
@@ -25,14 +26,17 @@ public class Dealer {
 	
 	public void dealCards() {
 		//deal Cards and update player and dealer hands
-		
-		
-		
-		
 		//set dealerHand for Table
 		this.table.setDealerHand(this.dealerHand);
 		//set showCard for Table
 		this.table.setDealerShowCard(this.seenCard);
+
+		// This will be a facade, calling getNextCard() and then sending that card to certain player.
+	}
+
+	public Card getNextCard(){
+		// Using the table object, call the method which accesses the shoe, which calls for the next card. Something like this i think.
+		return table.getShoe().dealNextCard();
 	}
 	
 	public void collectMoney() {
@@ -47,20 +51,22 @@ public class Dealer {
 	 * getters and setters below
 	 */
 	
-	public void setSeenCard(Card seenCard) {
+	/*public void setSeenCard(Card seenCard) {
 		this.seenCard = seenCard;
-	}
+	}*/
 	
 	public Card getSeenCard() {
-		return this.seenCard;
+		// Call hand method to return card.
+		return this.dealerHand.blahblahblahfillthisinwhenthefunctionismade;
 	}
 	
-	public void setUnseenCard(Card unseenCard) {
+	/*public void setUnseenCard(Card unseenCard) {
 		this.unseenCard = unseenCard;
-	}
+	}*/
 	
 	public Card getUnseenCard() {
-		return this.unseenCard;
+		// Call hand method to return card.
+		return this.dealerHand.blahblahblahfillthisinwhenthefunctionismade;
 	}
 	
 	public void setDealerHand(Hand dealerHand) {
