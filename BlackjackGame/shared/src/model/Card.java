a2fd07d738e5469cff601988c74165391be7a02f
Brandon Sandoval <74200474+BrandonSandoval@users.noreply.github.com>
2024-11-16 12:13:56 -0800

    Updated Card.java
    Added variable and code to make it work with hand

--- BlackjackGame/shared/src/model/Card.java
+++ BlackjackGame/shared/src/model/Card.java
@@ -1,19 +1,24 @@
 package model;
+
+import model.Card.CardRank;
+import model.Card.CardSuit;
+
 public class Card{
 	
-	private enum CardSuit{
+	public enum CardSuit{
 		Spades,
 		Hearts,
 		Clubs,
 		Diamonds;
 	}
 	
-	private enum CardRank{
+	public enum CardRank{
 		Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
 	}
 
 	private CardRank Rank;
 	private CardSuit Suit;
+	private int[] values = {-1, -1};
 	
 	public Card(int suitInt, int rankInt) {
 		switch(suitInt) {
@@ -23,28 +28,32 @@ public class Card{
 			case 3: this.Suit = CardSuit.Diamonds; break;
 		}
 		switch(rankInt) {
-			case 0: this.Rank = CardRank.Ace; break;
-			case 1: this.Rank = CardRank.Two; break;
-			case 2: this.Rank = CardRank.Three; break;
-			case 3: this.Rank = CardRank.Four; break;
-			case 4: this.Rank = CardRank.Five; break;
-			case 5: this.Rank = CardRank.Six; break;
-			case 6: this.Rank = CardRank.Seven; break;
-			case 7: this.Rank = CardRank.Eight; break;
-			case 8: this.Rank = CardRank.Nine; break;
-			case 9: this.Rank = CardRank.Ten; break;
-			case 10: this.Rank = CardRank.Jack; break;
-			case 11: this.Rank = CardRank.Queen; break;
-			case 12: this.Rank = CardRank.King; break;
+			case 1: values[0] = 1; values[1] = 11; this.Rank = CardRank.Ace; break;
+			case 2: values[0] = 2; this.Rank = CardRank.Two; break;
+			case 3: values[0] = 3; this.Rank = CardRank.Three; break;
+			case 4: values[0] = 4; this.Rank = CardRank.Four; break;
+			case 5: values[0] = 5; this.Rank = CardRank.Five; break;
+			case 6: values[0] = 6; this.Rank = CardRank.Six; break;
+			case 7: values[0] = 7; this.Rank = CardRank.Seven; break;
+			case 8: values[0] = 8; this.Rank = CardRank.Eight; break;
+			case 9: values[0] = 9; this.Rank = CardRank.Nine; break;
+			case 10: values[0] = 10; this.Rank = CardRank.Ten; break;
+			case 11: values[0] = 10; this.Rank = CardRank.Jack; break;
+			case 12: values[0] = 10; this.Rank = CardRank.Queen; break;
+			case 13: values[0] = 10; this.Rank = CardRank.King; break;
 		}
 	}
 
-	public Card.CardSuit getSuit() {
+	public CardSuit getSuit() {
 		return Suit;
 	}
 	
-	public Card.CardRank getRank(){
+	public CardRank getRank(){
 		return Rank;
+	}
+	
+	public int[] getValues() {
+		return this.values;
 	}
 	
 }
