bee4c7c0207970231e654b862ce8d10220c73a73
Carolina Barbecho <carolinabarbecho@outlook.com>
2024-11-30 15:39:44 -0800

    update unit tests

--- BlackjackGame/server/test/accountTest/AccountManagerTest.java
+++ BlackjackGame/server/test/accountTest/AccountManagerTest.java
@@ -4,26 +4,28 @@ import org.junit.Test;
 
 import account.AccountManager;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AccountManagerTest {
 	@Test
 	public void testLoginSuccess() {
 		//
-		assertTrue(AccountManager.getInstance().login("user1", "user1"));
+		assertNotEquals(AccountManager.getInstance().login("user1", "user1"), null);
 	}
 	
 	
 	@Test
 	public void testLoginFailUsername() {
 		//
-		assertFalse(AccountManager.getInstance().login("user981", "user1"));
+		assertEquals(AccountManager.getInstance().login("user981", "user1"), null);
 	}
 	
 	@Test
 	public void testLoginFailPassword() {
 		//
-		assertFalse(AccountManager.getInstance().login("user1", "wrongPassword"));
+		assertEquals(AccountManager.getInstance().login("user1", "wrongPassword"), null);
 	}
 }
